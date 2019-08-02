b688a7bf97aa04b8097f7e72ac9f3ffb0f099529
Glitch (lightweight-accounts-wip) <none>
2019-08-02 09:14:25 +0000

    📼💏 Checkpoint ./server.js:4438732/70

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/', function(request, response) {
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`Your app is listening on port ${listener.address().port}. ${new Date().toTimeString()}`);
 });
