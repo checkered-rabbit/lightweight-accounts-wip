8dc7ee90278f0187dd2ebfa09430ddbee8d15282
Glitch (lightweight-accounts-wip) <none>
2019-08-02 09:28:09 +0000

    😺🍢 Checkpoint ./accounts/db/users.js:4438732/348 ./views/index.ejs:4438732/106

--- accounts/db/users.js
+++ accounts/db/users.js
@@ -1,11 +1,15 @@
 // based on https://github.com/passport/express-4.x-local-example/blob/master/db/users.js
 
 var records = [
-  { id: 1, username: 'a', password: 'a', displayName: 'A', emails: [{ value: 'a@example.com' }] },
-  { id: 2, username: 'aa', password: 'aa', displayName: 'Aa', emails: [{ value: 'aa@example.com' }] },
+  { id: 1, username: 'a', password: 'a', displayName: 'A', emails: [{ value: 'a@example.com' }], data: {} },
+  { id: 2, username: 'aa', password: 'aa', displayName: 'Aa', emails: [{ value: 'aa@example.com' }], data: {} },
   {
-    id: 3, username: 'admin', password: process.env.SECRET, displayName: 'Admin',
-    emails: [{ value: 'aa@example.com' }]
+    id: 3, username: 'admin',
+    //do not allow empty or with spaces from env-vars, but that happens automatically
+    password: process.env.SECRET,
+    displayName: 'Admin',
+    emails: [{ value: 'admin@example.com' }],
+    data: { admin: true }
   },
 ];
 
